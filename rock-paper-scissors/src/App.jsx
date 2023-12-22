f07ff4777e7ce0db148a05122fbe18f7ab49e134
Gabriel <gabrielalves3654@gmail.com>
2023-12-22 11:16:51 -0300

    verify vite dependecies

---
+++ rock-paper-scissors/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+      <div>
+        <p>
+          Hello my old friend
+        </p>
+      </div>
+
+  )
+}
+
+export default App
