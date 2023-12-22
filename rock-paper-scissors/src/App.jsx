90da3297e11352d71164ef23c7210fb1a2c24fed
Gabriel <gabrielalves3654@gmail.com>
2023-12-22 11:38:16 -0300

    struct base page

--- rock-paper-scissors/src/App.jsx
+++ rock-paper-scissors/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0);
+
+
+  const game =  () => {
+    console.log("game");
+  }
 
   return (
       <div>
-        <p>
-          Hello my old friend
-        </p>
+        <button onClick={() => game()}>rock</button>
+        <button onClick={() => game()}>paper</button>
+        <button onClick={() => game()}>scissor</button>
+        <h3>You: 0</h3>
+        <h3>Computer: 0</h3>
       </div>
 
   )
