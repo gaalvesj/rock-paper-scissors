f40722984d3768745be1ff97741003c9b2bc2af2
Gabriel <gabrielalves3654@gmail.com>
2023-12-22 12:03:58 -0300

    handler logic machine in game

--- rock-paper-scissors/src/App.jsx
+++ rock-paper-scissors/src/App.jsx
@@ -1,18 +1,60 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [possibleMoves, setPossibleMoves] = useState([
+    {
+      type: 'paper',
+      label: 'paper',
+      wins: 'rock',
+      loses: 'scissors'
+    },
+    {
+      type: 'rock',
+      label: 'rock',
+      wins: 'scissors',
+      loses: 'paper'
+    },
+    {
+      type: 'scissors',
+      label: 'scissors',
+      wins: 'paper',
+      loses: 'rock'
+    }
+
+  ]);
+
+  
 
 
-  const game =  () => {
-    console.log("game");
+
+  const game =  (playerMove) => { 
+    console.log(playerMove);
+
+
+    const computerMove = {
+      get numero() {
+          return Math.floor(Math.random() * 3) + 1;
+      }
+  };
+  let computerMoveType = ''
+    switch(computerMove.numero){
+      case 1: 
+        computerMoveType = 'paper'
+      break;
+      case 2:
+        computerMoveType = 'rock'
+      break;
+      case 3:
+        computerMoveType = 'scissor';
+      break;
+    }
   }
 
   return (
       <div>
-        <button onClick={() => game()}>rock</button>
-        <button onClick={() => game()}>paper</button>
-        <button onClick={() => game()}>scissor</button>
+        <button onClick={() => game('rock')}>rock</button>
+        <button onClick={() => game('paper')}>paper</button>
+        <button onClick={() => game('scissor')}>scissor</button>
         <h3>You: 0</h3>
         <h3>Computer: 0</h3>
       </div>
