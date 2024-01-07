a62277ecc1528688e38ea7d7cd6521cbe8573ec4
Gabriel <gabrielalves3654@gmail.com>
2024-01-07 20:08:29 -0300

    create main page

--- rock-paper-scissors/src/App.jsx
+++ rock-paper-scissors/src/App.jsx
@@ -1,21 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Game from './components/Game'
-import Home from './components/Home';
-import Modal from './components/Modal';
+import ReactJoKenPo from "./pages/ReactJoKenPo";
 
 
 function App() {
  
   return (
-      <>
-      <Router>
-        <Routes>
-         <Route exact path='/' element={<Home />} />
-         <Route exact path='/game' element={<Game />} />
-         <Route exact path='/rules' element={<Modal />} />
-        </Routes>
-      </Router  >
-      </>
+    <>
+    
+    <ReactJoKenPo />
+    </>  
 
 
   )
---
+++ rock-paper-scissors/src/pages/ReactJoKenPo.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Game from './components/Game'
+import Home from './components/Home';
+import Modal from './components/Modal';
+
+
+function ReactJoKenPo() {
+
+
+    return(
+        <>
+      <Router>
+        <Routes>
+         <Route exact path='/' element={<Home />} />
+         <Route exact path='/game' element={<Game />} />
+         <Route exact path='/rules' element={<Modal />} />
+        </Routes>
+      </Router  >
+      </>
+    )
+}
+
+
+
+
+export default ReactJoKenPo;
